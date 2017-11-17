17dbf544dd5f49f87dadca75bba80778e322e0f3
Kyle Hoernlein <khoernlein90@gmail.com>
2017-11-16 23:16:04 -0500

    changed some js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,32 +7,32 @@ $(document).ready(function() {
     var intervalId;
     var clockRunning = false;
 
-     $(".unanswered").hide();
+    $(".unanswered").hide();
 
     function startTimer() {
         t = 10;
 
-            if (!clockRunning) {
-                t = 10;
-                intervalId = setInterval(decrement, 1000);
-                clockRunning = true;
-                $(".button").hide();
-                $(".gameInfo").hide();
-            }
-            if (clockRunning){
-                $(".snickleFritz").on("click", function(){
-                    resetTimer();
-                })
-            }
-            if (t === 0) {
-                incorrectAnswers++;
-                clockRunning = false;
-                stop();
+        if (!clockRunning) {
+            t = 10;
+            intervalId = setInterval(decrement, 1000);
+            clockRunning = true;
+            $(".button").hide();
+            $(".gameInfo").hide();
+        }
+        if (clockRunning) {
+            $(".answerButtons").on("click", function() {
+                resetTimer();
+            })
+        }
+        if (t === 0) {
+            incorrectAnswers++;
+            clockRunning = false;
+            stop();
 
-            }
+        }
     }
 
-    function resetTimer(){
+    function resetTimer() {
         clearInterval(intervalId);
         startTimer();
 
@@ -45,7 +45,7 @@ $(document).ready(function() {
             stop();
             questionIndex++;
             incorrectAnswers++;
-            $(".snickleFritz").empty();
+            $(".answerButtons").empty();
             postQuestion(questionIndex);
 
         }
@@ -96,47 +96,45 @@ $(document).ready(function() {
         question: "Which one of these films stars Robin Williams?",
         choices: ["Mrs. Doubtfire", "Black Swan", "The Hunt for Red October", "In Bruges"],
         answer: 0
-    },{
+    }, {
         question: "Which one of these films stars Robin Williams?",
         choices: ["The Bodyguard", "The Usual Suspects", "The World According to Garp", "Scott Pilgrim vs. the World"],
         answer: 2
-    },{
+    }, {
         question: "Which one of these films stars Robin Williams?",
         choices: ["21 Jump Street", "Awakenings", "Titanic", "American Beauty"],
         answer: 1
-    },{
+    }, {
         question: "Which one of these films stars Robin Williams?",
         choices: ["X-Men: First Class", "Dead Poets Society", "Dances with Wolves", "Before Sunset"],
         answer: 1
-    },{
+    }, {
         question: "Which one of these films stars Robin Williams?",
         choices: ["Batman", "The Avengers", "One Hour Photo", "King of New York"],
         answer: 2
-    },{
+    }, {
         question: "Which one of these films stars Robin Williams?",
         choices: ["Beasts of the Southern Wild", "Where the Heart Is", "Ghost", "The Fisher King"],
         answer: 3
     }];
 
     function postQuestion(post) {
-        $(".snickleFritz").off("click");
+        $(".answerButtons").off("click");
         if (questionIndex < questions.length) {
             startTimer();
             $(".questions").html("<h2>" + questions[post].question + "</h2>");
             for (var i = 0; i < questions[post].choices.length; i++) {
                 var newDiv = $("<div>");
-                newDiv.addClass("snickleFritz").attr("numberIndex", i).text(questions[post].choices[i]);
+                newDiv.addClass("answerButtons").attr("numberIndex", i).text(questions[post].choices[i]);
                 $(".choices").append(newDiv);
             }
+        } else {
+            endGame();
         }
-            else {
 
-                endGame();
-            }
-
-        $(".snickleFritz").on("click", function() {
+        $(".answerButtons").on("click", function() {
             console.log(this);
-            $(".snickleFritz").remove();
+            $(".answerButtons").remove();
             var userChoice = $(this).attr("numberIndex");
             userChoice = parseInt(userChoice);
 
@@ -147,13 +145,11 @@ $(document).ready(function() {
                 incorrectAnswers++;
                 questionIndex++;
             }
-
-                postQuestion(questionIndex);
-
+            postQuestion(questionIndex);
         })
     }
 
-    function startGame(){
+    function startGame() {
         var correctAnswers = 0;
         var incorrectAnswers = 0;
         var questionIndex = 0;
@@ -162,41 +158,39 @@ $(document).ready(function() {
             clearInterval(intervalId);
             startTimer();
             postQuestion(questionIndex);
-
         });
 
         $(".wins").empty();
         $(".losses").empty();
         $(".restartButton").hide();
         $(".button").show();
-
     }
 
     function endGame() {
-    stop();
-    $(".questions").empty();
-    $(".snickleFritz").empty();
-    $(".timer").hide();
-    $(".wins").html("<h3>Correct Answers: " + correctAnswers + "</h3>");
-    $(".losses").html("<h3>Incorrect Answers: " + incorrectAnswers + "</h3>");
-     $(".unanswered").show();
+        stop();
+        $(".questions").empty();
+        $(".answerButtons").empty();
+        $(".timer").hide();
+        $(".wins").html("<h3>Correct Answers: " + correctAnswers + "</h3>");
+        $(".losses").html("<h3>Incorrect Answers: " + incorrectAnswers + "</h3>");
+        $(".unanswered").show();
 
-    var restartButton = $("<button>");
-    restartButton.addClass("btn btn-primary btn-lg restartButton");
-    restartButton.text("Restart Game");
-    $(".unanswered").append($(restartButton));
-    $(".restartButton").on("click", function() {
+        var restartButton = $("<button>");
+        restartButton.addClass("btn btn-primary btn-lg restartButton");
+        restartButton.text("Restart Game");
+        $(".unanswered").append($(restartButton));
+        $(".restartButton").on("click", function() {
 
-        $(".restartButton").hide();
-        $(".wins").empty();
-        $(".losses").empty();
-        $(".timer").show();
-        correctAnswers = 0;
-        incorrectAnswers = 0;
-        questionIndex = 0;
+            $(".restartButton").hide();
+            $(".wins").empty();
+            $(".losses").empty();
+            $(".timer").show();
+            correctAnswers = 0;
+            incorrectAnswers = 0;
+            questionIndex = 0;
 
-        postQuestion(questionIndex);
-    })
+            postQuestion(questionIndex);
+        })
     }
 
     startGame();
